fcfa11ad77c7884a6ab0f12c686e84682a518a22
hhassan1230 <hhassan@oswego.edu>
2017-01-30 00:11:56 -0500

    adds explosion

---
+++ Assets/ShatterOnContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Exploder.Utils;
+
+public class ShatterOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "bullet") {
+			print ("I should explode!!!");
+			ExploderSingleton.ExploderInstance.ExplodeObject (this.gameObject);
+		}
+	}
+}
