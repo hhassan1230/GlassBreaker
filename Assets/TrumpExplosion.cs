05c4f016afd14eb33f062558da3e54bb654921a3
hhassan1230 <hhassan@oswego.edu>
2017-01-30 23:58:59 -0500

    adds Trump to the fun

---
+++ Assets/TrumpExplosion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Exploder.Utils;
+
+public class TrumpExplosion : MonoBehaviour {
+
+	void Awake()
+	{
+		//		exploder.CrackObject(obj1); // exploder is not defined
+//		Exploder.CrackedObject(this.gameObject); // Does not do anything
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "bullet") {
+			print ("I should explode!!!");
+			//			Exploder.ExplodeCracked(this.gameObject); // Errors
+//			ExploderSingleton.ExploderInstance.ExplodeObject (this.gameObject); // This works but takes too long
+		}
+	}
+}
